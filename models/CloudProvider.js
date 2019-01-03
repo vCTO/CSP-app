f545472ad00b3243b2ec1b6183cf811e9520f8cc
Edward Newman <ebnewman@gmail.com>
2019-01-03 22:07:42 +0000

    added EBA updates to schema

--- models/CloudProvider.js
+++ models/CloudProvider.js
@@ -1,87 +1,86 @@
 const mongoose = require('mongoose');
 
 const cspSchema = new mongoose.Schema({
-    cspName: {
-        type: String,
-        trim: true,
-    },
-    cspActivityType: {
-        type: String,
-        trim: true,
-    },
-    cspServiceDescription: {
-        type: String,
-        trim: true,
-    },
-    cspCountryDelivery: {
-        type: String,
-        trim: true,
-    },
-    cspServiceCommencementDate: {
-        type: Date,
-        trim: true,
-    },
-    cspContractLastRenewalDate: {
-        type: Date,
-        trim: true,
-    },
-    cspApplicableLaw: {
-        type: String,
-        trim: true,
-    },
-    cspContractNextRenewalDate: {
-        type: Date,
-        trim: true,
-    },
-    cspType: {
-        type: String,
-        trim: true,
-    },
-    cspParties: {
-        type: String,
-        trim: true,
-    },
-    cspApprovalEvidence: {
-        type: String,
-        trim: true,
-    },
-    cspGovernanceBodyName: {
-        type: String,
-        trim: true,
-    },
-    cspSubcontractors: {
-        type: String,
-        trim: true,
-    },
-    cspCountryRegister: {
-        type: String,
-        trim: true,
-    },
-    cspMaterialOutsource: {
-        type: String,
-        trim: true,
-    },
-    cspMaterialAssessmentDate: {
-        type: Date,
-        trim: true,
-    },
-    cspTimeCriticalSupport: {
-        type: String,
-        trim: true,
-    },
-    cspSubAssessment: {
-        type: String,
-        trim: true,
-    },
-    cspAltProvider: {
-        type: String,
-        trim: true,
-    },
-    cspLastRiskAssessmentDate: {
-        type: Date,
-        trim: true,
-    },
+  cspName: {
+    type: String,
+    trim: true,
+  },
+  cspActivityType: {
+    type: String,
+    trim: true,
+  },
+  cspServiceDescription: {
+    type: String,
+    trim: true,
+  },
+  cspCountryDelivery: {
+    type: String,
+    trim: true,
+  },
+  cspServiceCommencementDate: {
+    type: Date,
+    trim: true,
+  },
+  cspContractLastRenewalDate: {
+    type: Date,
+    trim: true,
+  },
+  cspApplicableLaw: {
+    type: String,
+    trim: true,
+  },
+  cspContractNextRenewalDate: {
+    type: Date,
+    trim: true,
+  },
+  cspType: {
+    type: String,
+    trim: true,
+  },
+  cspParties: {
+    type: String,
+    trim: true,
+  },
+  cspApprovalEvidence: {
+    type: String,
+    trim: true,
+  },
+  cspGovernanceBodyName: {
+    type: String,
+    trim: true,
+  },
+  cspSubcontractors: {
+    type: String,
+    trim: true,
+  },
+  cspCountryRegister: {
+    type: String,
+    trim: true,
+  },
+  cspMaterialOutsource: {
+    type: String,
+    trim: true,
+  },
+  cspMaterialAssessmentDate: {
+    type: Date,
+    trim: true,
+  },
+  cspTimeCriticalSupport: {
+    type: String,
+    trim: true,
+  },
+  cspSubAssessment: {
+    type: String,
+    trim: true,
+  },
+  cspAltProvider: {
+    type: String,
+    trim: true,
+  },
+  cspLastRiskAssessmentDate: {
+    type: Date,
+    trim: true,
+  },
 });
 
 module.exports = mongoose.model('CloudProvider', cspSchema);
-
